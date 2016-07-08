0e7ee9f0069062b46a4d633a2586eb000c58e947
Greg Linklater <greglink49@gmail.com>
2016-07-08 22:42:04 +0200

    refactor(dev): use express to serve development server

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var path = require('path')
+var cwd = process.cwd()
+var app = express()
+
+app.use('/bower_components', express.static('bower_components'))
+app.use('/css', express.static('css'))
+app.use('/img', express.static('img'))
+app.use('/js', express.static('js'))
+app.use('/pages', express.static('pages'))
+app.use('/img', express.static('img'))
+
+app.get('/:page', function (req, res, next) {
+  res.sendFile(path.join(cwd, 'index.html'))
+})
+
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(cwd, 'index.html'))
+})
+
+app.listen(3000)
